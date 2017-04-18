152315b2509da81c9d52d301eb0a6982c4f7a09c
Micadurp <neoswed@hotmail.com>
2017-04-18 11:08:58 +0200

    Basic recieve and send back stuff
    Needs more fixin

--- Nurn/Main.cpp
+++ Nurn/Main.cpp
@@ -1,7 +1,20 @@
 #include "Nurn.hpp"
 
+using namespace Nurn;
 int main()
 {
+	NurnEngine nurn = NurnEngine();
+	nurn.InitializeHost();
+
+	unsigned char * data = new unsigned char[2000];
+
+
+	//Receive a message from client
+	while (nurn.Receive(data, 2000) > 0)
+	{
+		//Send the message back to client
+		nurn.Send(data, 2000); // write(sock, client_message, strlen(client_message));
+	}
 
 	return 0;
 }
